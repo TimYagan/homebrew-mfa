f0ed569f41c662ded8acae1bfbdcf4cd5abe3bcd
Tim Yagan <30977192+TimYagan@users.noreply.github.com>
2026-04-01 00:48:26 +0200

    Update mfa formula to v0.1.2

--- Formula/mfa.rb
+++ Formula/mfa.rb
@@ -1,8 +1,8 @@
 class Mfa < Formula
   desc "Accurate, performant file size analysis tool for macOS"
   homepage "https://github.com/TimYagan/mac-file-analyzer"
-  url "https://github.com/TimYagan/mac-file-analyzer/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "cd1bf300760db230d282685e8f7bd01c4612d0e088d80092f3d49ef7e8aa38f9"
+  url "https://github.com/TimYagan/mac-file-analyzer/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "de50bd1a79b37b7a33c1f1078af3b58c3548167b850dad5099bf4140c1af9264"
   license "MIT"
 
   depends_on "rust" => :build
